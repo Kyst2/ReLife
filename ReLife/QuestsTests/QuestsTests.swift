3a80010c9006995c71a88a182ea8d4aade9ac7b1
AndrewKust <deradus@ukr.net>
2023-09-06 01:42:59 +0300

    Quests Today

--- ReLife/QuestsTests/QuestsTests.swift
+++ ReLife/QuestsTests/QuestsTests.swift
@@ -71,4 +71,69 @@ final class QuestsTests: XCTestCase {
         XCTAssertEqual(realmController.questsAll.first?.name, "VIE")
     }
     
+    func testGetQuestsToday() {
+        // first day of hear
+        // thurstday
+        let day3000 = Date.from(str: "3000/01/01")!.adding(hrs: 2)
+        let day3001 = Date.from(str: "3001/01/01")!
+        
+        let tmp1 = QuestRepeatType.singleDayQuest(date: day3000)                                 // will be
+        let tmp2 = QuestRepeatType.singleDayQuest(date: day3001)
+        
+        let tmp3 = QuestRepeatType.eachWeek(days: [4])                                           // will be
+        let tmp4 = QuestRepeatType.eachWeek(days: [1,2,6])
+        
+        let tmp5 = QuestRepeatType.dayOfMonth(days: [1])                                         // will be
+        let tmp6 = QuestRepeatType.dayOfMonth(days: [2,4,5,6])
+        
+        let tmp7 = QuestRepeatType.repeatEvery(days: 2, startingFrom: day3000.adding(days: -2) ) // will be
+        let tmp8 = QuestRepeatType.repeatEvery(days: 1, startingFrom: day3000)                   // will be
+        let tmp9 = QuestRepeatType.repeatEvery(days: 2, startingFrom: day3001)
+        let tmp10 = QuestRepeatType.repeatEvery(days: 3, startingFrom: day3000.adding(days: -2))
+        
+        
+        
+        var charachSet = [Characteristic : Int]()
+        let quest = Quest(name: "Quest1", icon: .backpack, color: .green, charachPoints: charachSet, questRepeatStr: tmp1)
+        realmController.add(quest: quest)
+        let quest2 = Quest(name: "Quest2", icon: .backpack, color: .green, charachPoints: charachSet, questRepeatStr: tmp2)
+        realmController.add(quest: quest2)
+        let quest3 = Quest(name: "Quest3", icon: .backpack, color: .green, charachPoints: charachSet, questRepeatStr: .repeatEvery(days: 5, startingFrom: Date.distantFuture))
+        realmController.add(quest: quest3)
+        
+        XCTAssertEqual(realmController.getQuestsToday(),
+                       [Quest(name: "Quest1", icon: .backpack, color: .green, charachPoints: charachSet, questRepeatStr: tmp1),
+                                                         
+                        Quest(name: "Quest2", icon: .backpack, color: .green, charachPoints: charachSet, questRepeatStr: tmp2)])
+        
+    }
+    
+}
+
+extension Date {
+    static func from(str: String, format: String = "yyyy'/'MM'/'dd") -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.date(from: str)
+    }
+    
+    func dayNumberOfWeek() -> Int? {
+        return Calendar.current.dateComponents([.weekday], from: self).weekday
+    }
+    
+    func adding(days: Int) -> Date {
+        return self.addingTimeInterval(TimeInterval(days*60*60*24))
+    }
+    
+    func adding(hrs: Int) -> Date {
+        return self.addingTimeInterval(TimeInterval(hrs*60*60))
+    }
+    
+    func adding(mins: Int) -> Date {
+        return self.addingTimeInterval(TimeInterval(mins*60))
+    }
+    
+    func adding(sec: Int) -> Date {
+        return self.addingTimeInterval(TimeInterval(sec))
+    }
 }
