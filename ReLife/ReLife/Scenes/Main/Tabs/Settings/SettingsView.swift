a3614417ff885e9d067c63c3781ff1a7675c0517
UKS <skulptorrr@gmail.com>
2023-12-28 06:24:34 +0200

    SettingsView optimized

--- ReLife/ReLife/Scenes/Main/Tabs/Settings/SettingsView.swift
+++ ReLife/ReLife/Scenes/Main/Tabs/Settings/SettingsView.swift
@@ -9,7 +9,7 @@ struct SettingsView: View {
         VStack(spacing: 0) {
             TabsPanel()
             
-            TabsBody()
+            TabContent()
                 .transition(AnyTransition.move(edge: .trailing))
         }
     }
@@ -18,19 +18,18 @@ struct SettingsView: View {
 extension SettingsView {
     func TabsPanel() -> some View {
         HStack(spacing: 0) {
-            ConfigTabView(tab: $model.tab, curr: .general)
+            SingleTab(.general)
             
-            ConfigTabView(tab: $model.tab, curr: .quests)
+            SingleTab(.quests)
             
-            ConfigTabView(tab: $model.tab, curr: .characteristics)
+            SingleTab(.characteristics)
             
-            ConfigTabView(tab: $model.tab, curr: .history)
+            SingleTab(.history)
         }
-        .id(SettingsTab.general.rawValue.localized) //fix refresh issues on locale change
     }
     
     @ViewBuilder
-    func TabsBody() -> some View {
+    func TabContent() -> some View {
         switch model.tab {
         case .general:
             ConfigGeneralView(model: model)
@@ -44,43 +43,47 @@ extension SettingsView {
     }
 }
 
-
-
-
-/////////////////
+//////////////////////////////
 ///HELPERS
-/////////////////
-//TODO: fix button to fill parent
-struct ConfigTabView: View {
-    @Binding fileprivate var tab: SettingsTab
-    fileprivate let curr: SettingsTab
-    
-    var body: some View {
-        Button(action: {
-            withAnimation(.easeIn(duration: 0.2 )){
-                tab = curr
-            }
-        }) {
-            Text(curr.rawValue.localized)
-                .myFont(size: 17)
-                .menuBttonModifier(tab: tab, curr: curr)
-                .id(curr.rawValue.localized)
-        }
-        .buttonStyle(.plain)
-    }
+//////////////////////////////
+enum SettingsTab: String {
+    case general = "key.settings.tab.general"
+    case quests = "key.quests"
+    case characteristics = "key.characteristics"
+    case history = "key.history"
 }
 
-fileprivate extension View {
-    func menuBttonModifier(tab: SettingsTab,curr:SettingsTab) -> some View{
-        self.frame(height: 40)
+extension SettingsView {
+    func SingleTab(_ curr: SettingsTab) -> some View {
+        Text(curr.rawValue.localized)
+            .myFont(size: 17)
+            .frame(height: 40)
             .frame(minWidth: 150, maxWidth: .infinity)
-            .contentShape(Rectangle())
-            .background {
-                tab == curr ? Color.gray.opacity(0.5) : Color.clear
+            .makeFullyIntaractable()
+            .overlay {
+                Rectangle()
+                    .stroke(Color.primary, lineWidth: 0.1)
             }
             .overlay {
-                RoundedRectangle(cornerRadius: 0)
-                    .stroke(Color.primary, lineWidth: 0.1)
+                VStack {
+                    VStack{
+                        Spacer()
+                        
+                        if model.tab == curr {
+                            RoundedRectangle(cornerRadius: 0)
+                                .fill(RLColors.brownLight)
+                                .frame(height: 3)
+                                .id("SettingTabSelection")
+                                .transition(.scale.combined(with: .opacity))
+                        }
+                    }
+                }
+                .animation(.easeInOut, value: model.tab)
+            }
+            .onTapGesture {
+                withAnimation(.easeIn(duration: 0.2 )){
+                    model.tab = curr
+                }
             }
     }
 }
