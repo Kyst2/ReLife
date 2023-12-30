95ee9421841d7c70f8880e185f4a8e2ec5a3f16e
UKS <skulptorrr@gmail.com>
2023-12-30 23:36:55 +0200

    Config commit does not work

---
+++ ReLife/ReLife/Config.swift
@@ -0,0 +1,23 @@
+import Foundation
+import SwiftUI
+import AsyncNinja
+import Essentials
+
+class Config : NinjaContext.Main  {
+    static let shared: Config = initSharedConfig()
+    
+    init(store: ConfigBackend) {
+        
+    }
+}
+
+////////////////////////
+///HELPERS
+///////////////////////
+
+fileprivate func initSharedConfig() -> Config {
+    let se: ServiceEnvironment = .Release
+    let cud = ConfigUserDefaults(env: se)
+    
+    return Config(store: cud)
+}
