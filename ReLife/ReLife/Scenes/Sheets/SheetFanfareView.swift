198ea105e8c553f0ea86330896b6486e9ee9e853
UKS <skulptorrr@gmail.com>
2024-01-02 09:48:09 +0200

    SheetFanfareView upd

--- ReLife/ReLife/Scenes/Sheets/SheetFanfareView.swift
+++ ReLife/ReLife/Scenes/Sheets/SheetFanfareView.swift
@@ -1,11 +1,12 @@
 import Foundation
 import SwiftUI
 import Essentials
+import MoreSwiftUI
 
 struct SheetFanfareView: View {
     @ObservedObject var model: SheetFanfareViewModel
     
-    let timer = TimerPublisher(every: 0.3)
+    let timer = TimerPublisher(every: 0.07)
     
     init(quest: Quest) {
         model = SheetFanfareViewModel(quest)
@@ -13,13 +14,23 @@ struct SheetFanfareView: View {
     
     var body: some View {
         CharacteristicsList(charsAndPoints: $model.charsAndPoints, spacings: false)
-//            .frame(minWidth: 300, minHeight: 250)
-//            .padding(30)
+            .paddingAlt([.all], value: 30)
+            .frame(minWidth: 400, minHeight: 300)
+            .background {
+                AuroraClouds(blur: 20, theme: AuroraTheme.reLifeAchievement)
+            }
             .onReceive(timer) { _ in
                 model.charsAndPoints.indices.forEach { idx in
-                    if model.charsAndPoints[idx].points <= model.charsAndPointsGoal[idx].points {
+                    if model.charsAndPoints[idx].points < model.charsAndPointsGoal[idx].points {
                         model.charsAndPoints[idx].points += 1
                     }
+                }
+            }
+            .onTapGesture {
+                if !model.charsAndPoints.isEqual(to: model.charsAndPointsGoal) {
+                    model.charsAndPoints = model.charsAndPointsGoal
+                } else {
+                    GlobalDialog.close()
                 }
             }
     }
@@ -30,17 +41,32 @@ class SheetFanfareViewModel: ObservableObject {
     @Published var charsAndPointsGoal: [CharacteristicsAndPoints]
     
     init (_ quest: Quest) {
-        self.charsAndPoints = RealmController.shared.getAllCharacteristicPoints()
-        var tmpCharsAndPointsGoal = RealmController.shared.getAllCharacteristicPoints()
+        let tmp = RealmController.shared.getAllCharacteristicPoints()
+        self.charsAndPoints = tmp
+        var tmpCharsAndPointsGoal = tmp
         
-        quest.charachPoints.forEach { questChPoints in
-            guard let item = tmpCharsAndPointsGoal.filter({ $0.id.uuidString == questChPoints.key }).first,
+        let questCharachPoints = quest.charachPoints.map{ ($0.key, $0.value) }
+        
+        questCharachPoints.forEach { questChPoints in
+            guard let item = tmpCharsAndPointsGoal.filter({ $0.charac.key == questChPoints.0 }).first,
                   let idx = tmpCharsAndPointsGoal.firstIndex(of: item)
             else { return }
             
-            tmpCharsAndPointsGoal[idx].points = item.points + questChPoints.value
+            tmpCharsAndPointsGoal[idx].points = item.points + questChPoints.1
         }
         
         charsAndPointsGoal = tmpCharsAndPointsGoal
+    }
+}
+
+fileprivate extension Array where Element == CharacteristicsAndPoints {
+    func isEqual(to: [CharacteristicsAndPoints]) -> Bool {
+        for idx in indices {
+            if self[idx] != to[idx] {
+                return false
+            }
+        }
+        
+        return true
     }
 }
