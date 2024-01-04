58a5cb43b499069a6f16094028dd8065ff74927b
UKS <skulptorrr@gmail.com>
2024-01-04 08:54:10 +0200

    Review request added on each app run (will work after publish to appStore)

--- ReLife/ReLife/BackendCore/AppDelegate.swift
+++ ReLife/ReLife/BackendCore/AppDelegate.swift
@@ -1,10 +1,15 @@
 import Foundation
 import SwiftUI
 import AppCoreLight
+import AppReview
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillFinishLaunching(_ notification: Notification) {
         hackToInitializeStaticValuesOnAppStart()
+    }
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        AppReview.requestIf(launches: 1)
     }
 }
 
